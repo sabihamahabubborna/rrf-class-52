20c54f30dc71e43fd1e9cc7430a32f460a136969
sabihamahabubborna <30389227+sabihamahabubborna@users.noreply.github.com>
2017-11-30 01:08:12 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+
+ $('.slider-area').owlCarousel({
+ 	items:1,
+ 	loop:true,
+ 	nav:false,
+ 	dots:true,
+ 	autoplay:false
+
+
+
+                
+            });
+ $('.logo-carousel').owlCarousel({
+ 	items:5,
+ 	loop:true,
+ 	nav:false,
+ 	dots:true,
+ 	autoplay:false
+
+
+
+                
+            });
+
+});
